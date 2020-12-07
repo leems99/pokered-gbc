328f823216b0df8c5fa5d3f606452f67ca0db988
leems99 <72859781+leems99@users.noreply.github.com>
2020-12-07 13:33:31 -0700

    Fixed run, bike, and surf
    Fixed B to run and faster biking and surfing from R++.

--- home/overworld.asm
+++ home/overworld.asm
@@ -282,12 +282,26 @@ OverworldLoopLessDelay::
 	ld a, [wd736]
 	bit 6, a ; jumping a ledge?
 	jr nz, .normalPlayerSpriteAdvancement
-	call DoBikeSpeedup
+	call DoBikeSpeedup ; if riding a bike and not jumping a ledge
+	call DoBikeSpeedup ; added
+	call DoBikeSpeedup ; added
+	jr .notRunning
 .normalPlayerSpriteAdvancement
+	; Make you surf at bike speed
+	ld a,[wWalkBikeSurfState]
+	cp a, $02
+	jr z, .surfFaster
+	; Add running shoes
+	ld a, [hJoyHeld] ; Check what buttons are being pressed
+	and B_BUTTON ; Are you holding B?
+	jr z, .notRunning ; If you aren't holding B, skip ahead to step normally.
+.surfFaster
+	call DoBikeSpeedup ; Make you go faster if you were holding B
+.notRunning ; Normal code resumes here
 	call AdvancePlayerSprite
-	ld a, [wWalkCounter]
+	ld a,[wWalkCounter]
 	and a
-	jp nz, CheckMapConnections ; it seems like this check will never succeed (the other place where CheckMapConnections is run works)
+	jp nz,CheckMapConnections ; it seems like this check will never succeed (the other place where CheckMapConnections is run works)
 ; walking animation finished
 	ld a, [wd730]
 	bit 7, a
